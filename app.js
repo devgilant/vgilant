cfed6bd33b3f53761af589d81b33db633966d8ee
Inbar Shani <devgilant@gmail.com>
2016-01-31 21:08:56 +0200

    setup basic site with config options

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var vgilant_config = require('./lib/config');
 
+var app = express();
+app.use(bodyParser.json());
+
+app.use(express.static('static'));
+
+/*
+app.get('/', function(req, res) {
+});
+*/
+
+app.listen(vgilant_config.WEB_APP_PORT,  function () {
+  console.log('\nVgilant app is listening at port '+vgilant_config.WEB_APP_PORT+'\n');
+});
